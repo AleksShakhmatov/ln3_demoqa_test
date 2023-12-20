c8337fcc388af8a142336dfd4bd46fc5107b3349
AShakhmatov <Coluchy@yandex.ru>
2023-12-20 09:20:50 +0600

    remove comments Configuration.pageLoadStrategy = "none"

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -14,12 +14,10 @@ public class PracticeFormTests {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.pageLoadStrategy = "none";
     }
 
     @Test
     void successfulRegistrationTest() {
-
         open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
         executeJavaScript("$('#fixedban').remove()");
@@ -28,18 +26,15 @@ public class PracticeFormTests {
         $("#lastName").setValue("Exile");
         $("#userEmail").setValue("AleksandrExile@gmail.com");
         $("#genterWrapper").$(byText("Male")).click();
-        //$("label[for=gender-radio-1").click();
         $("#userNumber").setValue("900112299");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOptionByValue("6");
         $(".react-datepicker__year-select").selectOption("1988");
         $(".react-datepicker__day--009:not(.react-datepicker__day--outside-month)").click();
         $("#subjectsInput").setValue("Arts").pressEnter();
-       // $("label[for=hohobbiesWrapperbbies-checkbox-1").click();
         $("#hobbiesWrapper").$(byText("Sports")).click();
         $("#uploadPicture").uploadFromClasspath("img/9.png");
         $("#currentAddress").setValue("www.Leningrad.spb.ru");
-        //$("#gender-radio-1").parent().click();
         $("#state").click();
         $("#stateCity-wrapper").$(byText("Rajasthan")).click();
         $("#city").click();
